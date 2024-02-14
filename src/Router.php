591a9012a72b453bcda2d09600aa64bab62454c4
Leonardo Sobral <leonardo98barros@gmail.com>
2024-02-13 23:32:40 -0300

    chore: improve Router.php file adding doc and try catch block

--- src/Router.php
+++ src/Router.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+use Exception;
+use Throwable;
+
 class Router
 {
     /**
@@ -9,34 +12,89 @@ class Router
      */
     protected $routes = [];
 
-    private function addRoute($route, $controller, $action, $method)
+    /**
+     * @param string $route
+     * @param string $controller
+     * @param string $action
+     * @param string $method
+     * 
+     * @return void
+     */
+    private function addRoute($route, $controller, $action, $method): void
     {
         $this->routes[$method][$route] = ['controller' => $controller, 'action' => $action];
     }
 
-    public function get($route, $controller, $action)
+    /**
+     * @param string $route
+     * @param string $controller
+     * @param string $action
+     * 
+     * @return void
+     */
+    public function get($route, $controller, $action): void
     {
         $this->addRoute($route, $controller, $action, "GET");
     }
 
-    public function post($route, $controller, $action)
+    /**
+     * @param string $route
+     * @param string $controller
+     * @param string $action
+     * 
+     * @return void
+     */
+    public function post($route, $controller, $action): void
     {
-        $this->addRoute($route, $controller, $action, "GET");
+        $this->addRoute($route, $controller, $action, "POST");
     }
 
-    public function dispatch()
+    /**
+     * @param string $route
+     * @param string $controller
+     * @param string $action
+     * 
+     * @return void
+     */
+    public function put($route, $controller, $action): void
+    {
+        $this->addRoute($route, $controller, $action, "POST");
+    }
+
+    /**
+     * @param string $route
+     * @param string $controller
+     * @param string $action
+     * 
+     * @return void
+     */
+    public function delete($route, $controller, $action): void
+    {
+        $this->addRoute($route, $controller, $action, "POST");
+    }
+
+    /**
+     * Dispacth route action
+     * 
+     * @return void
+     */
+    public function dispatch(): void
     {
         $uri = strtok($_SERVER['REQUEST_URI'], '?');
         $method =  $_SERVER['REQUEST_METHOD'];
 
-        if (array_key_exists($uri, $this->routes[$method])) {
+        try {
+            if (! array_key_exists($uri, $this->routes[$method])) {
+                throw new Exception("No route found for URI: $uri", 404);   
+            }
+
             $controller = $this->routes[$method][$uri]['controller'];
             $action = $this->routes[$method][$uri]['action'];
 
             $controller = new $controller();
             $controller->$action();
-        } else {
-            throw new \Exception("No route found for URI: $uri");
+        } catch (Throwable $th) {
+            include "Views/Errors/{$th->getCode()}.php";
         }
     }
 }
